4572289fd543d91b24926cbaed6b2480d2df8119
Nick Colijn <nickcolijn2006@gmail.com>
2025-02-12 10:53:33 +0100

    Eindhoven weather

---
+++ weerapp/resources/views/welcome.blade.php
@@ -0,0 +1,8 @@
+<x-app-layout>
+    <div class="min-h-screen flex flex-col justify-center items-center text-2xl font-bold">
+        <h1>Het weer in Eindhoven</h1>
+        <div class="mt-4 rounded-lg overflow-hidden">
+            <iframe src="//gadgets.buienradar.nl/gadget/forecastandstation/6370/" noresize scrolling=no hspace=0 vspace=0 frameborder=0 marginheight=0 marginwidth=0 width=300 height=190></iframe>
+        </div>
+    </div>
+</x-app-layout>
